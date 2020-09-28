f2ef1c444cf22f1748fe993a496c333a5c2b2c26
krishpranav <krisna.pranav@gmail.com>
2020-09-28 14:12:06 +0530

    done more changes in dogpile.py

--- plugins/dogpile.py
+++ plugins/dogpile.py
@@ -8,10 +8,9 @@ def search(domain, limit):
     app_emailharvester.process()
     return app_emailharvester.get_emails()
 
+    
 class Plugin:
-    def __init__(self, app, conf):#
+    def __init__(self, app, conf):
         global app_emailharvester, config
-        #config = conf
-        app.register_plugin('dogpile', {'search': search})
+        app.register_plugin('dogpile', {'search' : search})
         app_emailharvester = app
-        
