4c429c73cd7a2a48bf4aaec232837f24ee3d152d
krishpranav <krisna.pranav@gmail.com>
2020-09-28 13:09:34 +0530

    done more changes

--- plugins/dogpile.py
+++ plugins/dogpile.py
@@ -1,14 +1,12 @@
 #!usr/bin/env/python
-#config = None
-app_emailharvester = None
 
+app_emailharvester = None
 
 def search(domain, limit):
     url = 'http://www.dogpile.com/search/web?qsi={counter}&q="%40{word}"'
-    app_emailharvester.init_search(url, domain, limit, 1, 10, 'Dogpile')
+    app_emailharvester.__init__search(url, domain, limit, 1, 10, 'Dogpile')
     app_emailharvester.process()
     return app_emailharvester.get_emails()
-
 
 class Plugin:
     def __init__(self, app, conf):#
