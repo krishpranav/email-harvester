620bb73e69171407abe2916990b995d8f032b1e9
krishpranav <krisna.pranav@gmail.com>
2020-09-29 10:28:16 +0530

    done changes

--- EmailHarvester.py
+++ EmailHarvester.py
@@ -74,9 +74,10 @@ class EmailHarvester(object):
     def register_plugin(self, search_method, functions):
         self.plugins[search_method] = functions
 
+
     def get_plugins(self):
         return self.plugins
-
+        
     def show_message(self, msg):
         print(green(msg))
 
