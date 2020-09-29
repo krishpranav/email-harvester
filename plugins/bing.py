6ac3d070103ac9b0d973ff61444642ce4c83410c
krishpranav <krisna.pranav@gmail.com>
2020-09-29 13:37:23 +0530

    done changes in bing.py

--- plugins/bing.py
+++ plugins/bing.py
@@ -8,11 +8,8 @@ def search(domain, limit):
     app_emailharvester.process()
     return app_emailharvester.get_emails()  
 
-
-# class Plugin:
-#     def __init__(self, app, conf):
-#         global app_emailharvester, config
-#         #config = conf
-#         app.register_plugin('bing', {'search': search})
-#         app_emailharvester = app
-        
+class Plugin:
+    def __init__(self, app, conf):
+        global app_emailharvester, config
+        app.register_plugin('bing', {'search': search})
+        app_emailharvester = app
