86436a463999829a2270aa2b27b8b4434217344c
krishpranav <krisna.pranav@gmail.com>
2020-09-30 10:09:50 +0530

    done changes in exalead.py

--- plugins/exalead.py
+++ plugins/exalead.py
@@ -3,7 +3,7 @@ app_emailharvester = None
 
 
 def search(domain, limit):
-    url = "http://www.exalead.com/search/web/results/?q=%40{word}&elements_per_page=10&start_index={counter}" 
+    url = "http://www.exalead.com/search/web/results/?q=%40{word}&elements_per_page=10&start_index={counter}"
     app_emailharvester.init_search(url, domain, limit, 0, 50, 'Exalead')
     app_emailharvester.process()
     return app_emailharvester.get_emails()
