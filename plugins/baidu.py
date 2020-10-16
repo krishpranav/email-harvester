5d203b41a903c737488c4d9c2a49c07909b288c4
krishpranav <krisna.pranav@gmail.com>
2020-10-16 19:17:18 +0530

    done changes

--- plugins/baidu.py
+++ plugins/baidu.py
@@ -1,18 +1,14 @@
 #!usr/bin/env/python
 app_emailharvester = None
 
-
 def search(domain, limit):
     url = 'http://www.baidu.com/search/s?wd="%40{word}"&pn={counter}'
     app_emailharvester.init_search(url, domain, limit, 0, 10, 'Baidu')
     app_emailharvester.process()
     return app_emailharvester.get_emails()
 
-
 class Plugin:
-    def __init__(self, app, conf):#
+    def __init__(self, app, conf):
         global app_emailharvester, config
-        #config = conf
         app.register_plugin('baidu', {'search': search})
         app_emailharvester = app
-        
