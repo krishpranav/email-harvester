6854d60c6557277f8d5f0aa2e1241b504540fe21
krishpranav <krisna.pranav@gmail.com>
2020-10-17 09:14:12 +0530

    done changes

--- plugins/googleplus.py
+++ plugins/googleplus.py
@@ -1,1 +1,8 @@
 #!usr/bin/env/python
+app_emailharvester = None
+
+def search(domain, limit):
+    url = 'https://www.google.com/search?num=100&start={counter}&hl=en&q=site%3Aplus.google.com+intext:"Works at"+-inurl:photos+-inurl:about+-inurl:posts+-inurl:plusones+%40{word}'
+    app_emailharvester.init_search(url, domain, limit, 0, 100, 'Google+')
+    app_emailharvester.process()
+    return app_emailharvester.get_emails()
