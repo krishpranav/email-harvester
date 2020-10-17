f5f03d5ba8017d99ea5642d3bc96202c5a8621ba
krishpranav <krisna.pranav@gmail.com>
2020-10-17 13:37:12 +0530

    done changes

--- plugins/googleplus.py
+++ plugins/googleplus.py
@@ -2,13 +2,13 @@
 app_emailharvester = None
 
 def search(domain, limit):
-    url = 'https://www.google.com/search?num=100&start={counter}&hl=en&q=site%3Aplus.google.com+intext:"Works at"+-inurl:photos+-inurl:about+-inurl:posts+-inurl:plusones+%40{word}'
-    app_emailharvester.init_search(url, domain, limit, 0, 100, 'Google+')
-    app_emailharvester.process()
-    return app_emailharvester.get_emails()
+	url = 'https://www.google.com/search?num=100&start={counter}&hl=en&q=site%3Aplus.google.com+intext:"Works at"+-inurl:photos+-inurl:about+-inurl:posts+-inurl:plusones+%40{word}'
+	app_emailharvester.init_search(url, domain, limit, 0, 100, 'Google+')
+	app_emailharvester.process()
+	return app_emailharvester.get_emails()
 
 class Plugin:
-    def __init__(self, app, conf):
-        global app_emailharvester, config
-        app.register_plugin('googleplus', {'search': search})
-        app_emailharvester = app
+	def __init__(self, app, conf):
+		global app_emailharvester, config
+		app.register_plugin('googleplus', {'search': search})
+		app_emailharvester = app
