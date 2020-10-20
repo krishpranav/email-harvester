a9a7a7097ae6494e1b1f2916f2770653fdaae633
krishpranav <krisna.pranav@gmail.com>
2020-10-20 19:33:50 +0530

    done changes

--- plugins/googles.py
+++ plugins/googles.py
@@ -2,10 +2,10 @@
 app_emailharvester = None
 
 def search(domain, limit):
-	url = 'https://www.google.com/search?num=100&start={counter}&hl=en&q="%40{word}"'
-	app_emailharvester.init_search(url, domain, limit, 0, 100, 'Google')
-	app_emailharvester.process()
-	return app_emailharvester.get_emails()
+    url = 'https://www.google.com/search?num=100&start={counter}&hl=en&q="%40{word}"'
+    app_emailharvester.init_search(url, domain, limit, 0, 100, 'Google')
+    app_emailharvester.process()
+    return app_emailharvester.get_emails()
 
 class Plugin:
     def __init__(self, app, conf):
