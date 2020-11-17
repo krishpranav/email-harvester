cd2644e51720432723c8259e10836a28d9af949d
krishpranav <krisna.pranav@gmail.com>
2020-11-17 17:17:42 +0530

    done changes

--- plugins/dogpile.py
+++ plugins/dogpile.py
@@ -8,6 +8,7 @@ def search(domain, limit):
     app_emailharvester.process()
     return app_emailharvester.get_emails()
 
+
 class Plugin:
     def __init__(self, app, conf):
         global app_emailharvester, config
