d9ef7aaa5ffa345874e24df6466302f281fdf258
krishpranav <krisna.pranav@gmail.com>
2020-11-30 15:53:56 +0530

    done changes

--- plugins/bing.py
+++ plugins/bing.py
@@ -2,7 +2,7 @@
 app_emailharvester = None
 
 def search(domain, limit):
-    url ='http://www.bing.com/search?q=%40{word}&count=50&first={counter}'
+    url = 'http://www.bing.com/search?q=%40{word}&count=50&first={counter}'
     app_emailharvester.init_search(url, domain, limit, 0, 50, 'Bing')
     app_emailharvester.process()
     return app_emailharvester.get_emails()
